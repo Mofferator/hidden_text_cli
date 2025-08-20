c96a9f3fa96c10f97660e8c9ca0bec56acaf574d
Mifferator <moff@fake-email.com>
2025-08-20 14:42:25 +0000

    Added variable logging and options for user defined low and high chars

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::{fs, path::PathBuf, str::FromStr};
 use clap::{Parser, Subcommand};
 use hidden_text::{encode_hidden, decode_hidden};
+use env_logger;
+use log::LevelFilter;
+use atty::Stream;
 
 #[derive(Parser, Debug)]
 #[command(name = "hidden", version, about = "Decodes/Encodes text into zero width characters")]
 struct Cli {
     #[clap(subcommand)]
-    command: Commands
+    command: Commands,
+
+    #[clap(flatten)]
+    global_flags: GlobalFlags
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    #[command(about = "Encode a string into hidden text")]
     Encode {
         #[arg(short = 'o', long = "output")]
         output: Option<String>,
@@ -21,22 +28,59 @@ enum Commands {
         #[arg(short = 'c', long = "copy")]
         copy: bool,
 
+        #[arg(short = 'l', long = "low")]
+        low_char: Option<char>,
+
+        #[arg(short = 'h', long = "high")]
+        high_char: Option<char>,
+
         text: String
     },
 
+    #[command(about = "Extract and decode hidden text from a string")]
     Decode {
-        text: String
+        text: String,
+
+        #[arg(short = 'l', long = "low")]
+        low_char: Option<char>,
+
+        #[arg(short = 'h', long = "high")]
+        high_char: Option<char>,
     }
 }
 
+#[derive(Parser, Debug)]
+struct GlobalFlags {
+    #[clap(short, long, default_value_t=false)]
+    verbose: bool
+}
+
+
 fn main() {
     let args = Cli::parse();
+
+    // set verbosity level
+    let level = match (atty::is(Stream::Stdout), args.global_flags.verbose) {
+        (true, _) => LevelFilter::Off,
+        (_, false) => LevelFilter::Warn,
+        (_, true) => LevelFilter::Info
+    };
+    env_logger::builder()
+        .format_timestamp(None)
+        .filter_level(level)
+        .init();
+
     match args.command {
-        Commands::Encode { output, copy, text, plain_text } => {
-            let mut encoded_text = match encode_hidden(text) {
+        Commands::Encode { output, 
+                            copy, 
+                            text, 
+                            plain_text, 
+                            low_char, 
+                            high_char } => {
+            let mut encoded_text = match encode_hidden(text, low_char, high_char) {
                 Some(text) => text,
                 None => {
-                    eprintln!("No text provided");
+                    log::warn!("No text provided");
                     return;
                 }
             };
@@ -47,24 +91,24 @@ fn main() {
                 let path:PathBuf = match PathBuf::from_str(output.as_str()) {
                     Ok(path) => path,
                     Err(_) => {
-                        eprintln!("Invalid path");
+                        log::warn!("Invalid path");
                         return
                     }
                 };
                 match fs::write(&path, encoded_text) {
-                    Ok(_) => {println!("Wrote encoded string to {}", path.to_str().unwrap_or(""))},
-                    Err(err) => {eprintln!("Failed to write file to {}\nError: {err}", path.to_str().unwrap_or(""))}
+                    Ok(_) => {log::info!("Wrote encoded string to {}", path.to_str().unwrap_or(""))},
+                    Err(err) => {log::warn!("Failed to write file to {}\nError: {err}", path.to_str().unwrap_or(""))}
                 }
             }
             if copy {
                 println!("This is where we would copy");
             }
         },
-        Commands::Decode { text } => {
-            match decode_hidden(text) {
+        Commands::Decode { text, low_char, high_char } => {
+            match decode_hidden(text, low_char, high_char) {
                 Ok(Some(text)) => println!("{text}"),
-                Ok(None) => eprintln!("No hidden text found"),
-                Err(err) => eprintln!("Error decoding hidden text: {}", err)
+                Ok(None) => log::warn!("No hidden text found"),
+                Err(err) => log::warn!("Error decoding hidden text: {}", err)
             };
         }
     }   
